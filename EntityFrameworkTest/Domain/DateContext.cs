48d8192718006732956c23ab4b58c430fbcda8fe
danieltabares <danitabares5@hotmail.com>
2017-06-12 12:11:57 -0500

    First Version

---
+++ EntityFrameworkTest/Domain/DateContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DateContext : DbContext
+    {
+        public DateContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Domain.League> Leagues { get; set; }
+    }
+}
